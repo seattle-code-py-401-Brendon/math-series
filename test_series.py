a704c7ad167bd9597924694151abfec89456f259
Brendon <Brendonhampton1988@gmail.com>
2022-05-27 21:53:23 -0700

    refactored tests to reflect pythonic language

--- test_series.py
+++ test_series.py
@@ -1,47 +1,37 @@
 import pytest
-import series
-
-
-
-#get a initial failing test
-def test_initial_fail():
-    one = True
-    two = False
-    assert series.failing(one) == True
-#get a initial passing test
-def test_initial_pass():
-    one = True
-    two = False
-    assert series.passing(two) != True
+from series import (
+    fibonacci,
+    lucas
+)
 
 
 #Fibbonacci tests
 def test_fibbonacci_0():
     fibbNum = 0
-    assert series.fibonacci(fibbNum) == 0
+    assert fibonacci(fibbNum) == 0
 
 def test_fibonacci_pass():
     fibbNum = 5
-    assert series.fibonacci(fibbNum) == 5
+    assert fibonacci(fibbNum) == 5
 
 def test_fibonacci_fail():
     fibbNum = 5
-    assert series.fibonacci(fibbNum) != 4
+    assert fibonacci(fibbNum) != 4
 
 #Lucas tests
 
 def test_lucas_0():
     lucaNum = 0
-    assert series.lucas(lucaNum) == 2
+    assert lucas(lucaNum) == 2
 
 def test_lucas_pass():
     lucaNum = 1
-    assert series.lucas(lucaNum) == 1
+    assert lucas(lucaNum) == 1
 
 def test_lucas_fail():
     lucaNum = 3
-    assert series.lucas(lucaNum) != 3
+    assert lucas(lucaNum) != 3
 
 def test_lucas_pass():
     lucaNum = 4
-    assert series.lucas(lucaNum) == 7
+    assert lucas(lucaNum) == 7
